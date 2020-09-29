a8d65d5fdd937c4b95c3bfd887edf1bd02d1c976
jendrek <jedrekmiecznikowski@users.noreply.github.com>
2020-09-29 18:15:24 +0200

    Initial commit

---
+++ AU-Challenge-Grouping-Prototype.R
@@ -0,0 +1,95 @@
+
+# I use pacman for package management, but you can do the regular install.packages and library/require
+require(pacman)
+pacman::p_load(tidyverse, groupdata2, anticlust)
+
+
+# simulated data
+data_sim <- tibble(id = seq(1, 144, 1),
+                   faculty = c(rep("Arts", 45), rep("BSS", 64), rep("ST", 35)),
+                   dominant = sample(1:4, 144, replace = T))
+
+# not gonna be useful, I'll simulate some based on the few entries Merethe sent me
+
+
+#part data
+
+setwd("C:/Users/Jedrek/Documents/Github/Grouping-participants/") # dir
+
+data_part <- read.csv2("test_participants2.csv") # dataset from merethe (same structure as the final one but fewer participants)
+
+View(data_part) # looksie
+
+str(data_part) # looksie into structure
+
+data_part <- data_part[rep(seq_len(nrow(data_part)), each = 5), ] # everything 5 times because going to work on larger dataframe
+
+data_part$Faculty <- as.factor(data_part$Faculty) # gotta change that to factor
+
+rownames(data_part) <- seq(1,135,1) # replicate creates messy rownames, clean it up
+
+## OBS! ALGORITHM WORKS FOR K FOLDS WHERE K_MAX IS MIN NUMBER OF STUDENTS FROM SMALLEST FACULTY!!!! ##
+
+## probably going to have to join ST back together for that reason
+
+# first count
+nrow(data_part %>% filter(Faculty=="Natural Sciences" | Faculty == "Technical Sciences"))
+# 20 groups might be good enough and that way I'll test if the sizes are equal (crossing fingers)
+
+## something to ask: is health still a thing?? # apparently it is (checked sign up page)
+
+# new col with faculties health, technical and natural sciences together
+data_part <- data_part %>% mutate("Faculty_grouped" = ifelse(Faculty=="Natural Sciences" | Faculty == "Technical Sciences" | Faculty == "Health", "AaaaNatur|Technic|Health", as.character(Faculty)),
+                                  "Faculty_grouped2" = ifelse(Faculty=="Natural Sciences" | Faculty == "Technical Sciences" | Faculty == "Health" | Faculty == "Arts", "Arts|Natur|Technic|Health", as.character(Faculty)))
+# did it work
+plot(as.factor(data_part$Faculty_grouped))
+plot(as.factor(data_part$Faculty_grouped2))
+# arrange and then take a nice round number 
+data_part <- data_part %>% arrange(Faculty_grouped)
+set.seed(123) # for reproducibility
+# folding twice because once I put all the small faculties together (everything other than bss) and once I leave arts stand on its own
+# obv its better to randomize with arts on its own but algorithm might break, therefore test both
+# i use first 80 observations only because in my fake dataset Natural and Technical give 20
+data_folded <-  fold(data_part[1:80,], k = 4, method = "greedy", cat_col = "Faculty_grouped")
+data_folded2 <-  fold(data_part[1:80,], k = 4, method = "greedy", cat_col = "Faculty_grouped2")
+
+# different methods, I use greedy, where each fold grabs as many elements as possible (up to the specified size), meaning that there might be less elements available to the last group, but that all other groups than the last are guaranteed to have the size specified.
+
+# from now on I will use fold and group interchangeably
+
+# see if it worked
+
+# arts alone
+GROUPED <- data_folded %>%
+  group_by(.folds) %>%
+  summarise(n = n())
+plot(GROUPED)
+
+# arts blended in
+GROUPED2 <- data_folded2 %>%
+  group_by(.folds) %>%
+  summarise(n = n())
+plot(GROUPED2)
+# yes!
+# arrange back by folds for easy reading
+data_folded <- data_folded %>% arrange(.folds)
+
+# look at a random fold 
+data_folded %>% select(Faculty_grouped, .folds) %>% filter(.folds == 12)
+
+
+## ok, we have problems here but maybe I can cluster it now to balance the groups xd
+
+# anticlust is a random package for creating balanced clusters
+# need to make .folds numeric for clustering to work
+data_folded2$.folds <- as.numeric(data_folded2$.folds)
+# cluster into a vector
+bc <- balanced_clustering(data_folded2 %>% select(.folds), 20)
+# add new column to the data set
+data_folded2$NEW <- bc
+# check even groups visually
+plot(as.factor(data_folded2$NEW))
+# yay that works and groups are even
+
+# now for clarity I will append the dropped observations (if it doesn't div by 4) to the frame
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Grouping-participants
+ Grouping participants into evenly sized groups that are as heterogenous as possible for a local case competition.
